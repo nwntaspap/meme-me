8d9a926287397273b77c79ad1fe5d046c45506c3
nwntaspap <nwntaspapmichael@gmail.com>
2026-04-19 15:55:58 +0300

    main now is using modal to show dialogs, and insert explicit content to each one based on functionality

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css';
 import { drawVideo, getVideo } from './assets/camera';
 import { drawText } from './assets/text';
 import { getThemeToggle } from './assets/theme';
+import { Modal } from './assets/modal';
 
 const memeCanvas = document.getElementById('meme');
 
@@ -19,26 +20,67 @@ for (let canvas of [selfieLayer, textLayer]) {
 // meme with the layers' new data
 function redrawCanvas() {
   const memeCtx = memeCanvas.getContext('2d');
-  memeCtx.drawImage(selfieLayer, 0, 0);
-  memeCtx.drawImage(textLayer, 0, 0);
+  memeCtx.drawImage(selfieLayer, 0, 0); // Bottom Layer
+  memeCtx.drawImage(textLayer, 0, 0); // Top Layer
 }
 
-const previewCanvas = document.getElementById('preview');
-const savePhotoBtn = document.getElementById('save-photo');
-const video = await getVideo(previewCanvas);
-savePhotoBtn.addEventListener('click', () => {
-  drawVideo(video, selfieLayer);
-  redrawCanvas();
-});
+function setupSettings() {
+  const settings = document.getElementById('settings');
+  getThemeToggle();
 
-const saveTextBtn = document.getElementById('text-save');
-saveTextBtn.addEventListener('click', () => {
-  drawText(textLayer);
-  redrawCanvas();
+  const themeModal = new Modal(
+    'Settings',
+    settings,
+    settings.querySelector('.modal-content'),
+  );
+  themeModal.render();
+}
 
-  // Clear the input fields
-  document.getElementById('text-top').value = '';
-  document.getElementById('text-bottom').value = '';
-});
+function setupAddText() {
+  const textInputs = document.getElementById('add-text');
+  const saveTextBtn = document.getElementById('text-save');
 
-getThemeToggle();
+  saveTextBtn.addEventListener('click', () => {
+    drawText(textLayer);
+    redrawCanvas();
+
+    // Clear the input fields
+    document.getElementById('text-top').value = '';
+    document.getElementById('text-bottom').value = '';
+  });
+
+  const textModal = new Modal(
+    'Add some text',
+    textInputs,
+    textInputs.querySelector('.modal-content'),
+  );
+  textModal.render();
+}
+
+async function setupTakeSelfie() {
+  const selfie = document.getElementById('take-selfie');
+  const savePhotoBtn = document.getElementById('save-photo');
+
+  const selfieModal = new Modal(
+    'Take a selfie',
+    selfie,
+    selfie.querySelector('.modal-content'),
+  );
+  selfieModal.render();
+
+  const previewCanvas = document.getElementById('preview');
+
+  const video = await getVideo(previewCanvas);
+
+  savePhotoBtn.addEventListener('click', () => {
+    drawVideo(video, selfieLayer);
+    redrawCanvas();
+  });
+}
+
+// IIFE in case we don't have top-level await
+(async function run() {
+  setupSettings();
+  setupAddText();
+  await setupTakeSelfie();
+})();
